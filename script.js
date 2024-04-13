b604b9e7fad9f101b2d0bdabffebef0a59ac85af
Jacob Samuel <imjacobsamuel@gmail.com>
2024-04-13 09:07:42 +0530

    Added food item in the game area

--- script.js
+++ script.js
@@ -13,7 +13,39 @@ document.addEventListener("DOMContentLoaded", () => {
   let dx = cellSize;
   let dy = 0;
 
-  function startGame() {
+  function drawScoreBoard() {
+    const scoreBoard = document.getElementById("score-board");
+    scoreBoard.textContent = `Score : ${score}`;
+  }
+
+  function drawDiv(x, y, className) {
+    const div = document.createElement("div");
+    div.classList.add(className);
+    div.style.top = `${y}px`;
+    div.style.left = `${x}px`;
+    return div;
+  }
+
+  function drawFoodAndSnake() {
+    gameArena.innerHTML = "";
+
+    const foodElement = drawDiv(food.x, food.y, "food");
+    gameArena.appendChild(foodElement);
+  }
+
+  function gameLoop() {
+    setInterval(() => {
+      drawScoreBoard();
+      drawFoodAndSnake();
+    }, 1000);
+  }
+
+  function runGame() {
+    gameStarted = true;
+    gameLoop();
+  }
+
+  function initiateGame() {
     const scoreBoard = document.createElement("div");
     scoreBoard.id = "score-board";
     document.body.insertBefore(scoreBoard, gameArena);
@@ -22,7 +54,12 @@ document.addEventListener("DOMContentLoaded", () => {
     startButton.textContent = "Start Game";
     startButton.classList.add("start-button");
     document.body.appendChild(startButton);
+
+    startButton.addEventListener("click", () => {
+      startButton.style.display = "none";
+      runGame();
+    });
   }
 
-  startGame();
+  initiateGame();
 });
